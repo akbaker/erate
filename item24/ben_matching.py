34dbe63736b14a1340c1a8b50473dc7aedb71481
akbaker <FCC@FCCs-MacBook-Pro.local>
2014-05-23 12:21:33 -0400

    Item 24 sorting & matching

---
+++ item24/ben_matching.py
@@ -0,0 +1,88 @@
+## ---------------------------------------------------------------------------
+### VERSION 0.1 (for postgis)
+### ben_matching.py
+### Created on: May 23, 2013
+### Created by: Allison Baker
+### Federal Communications Commission 
+##
+## ---------------------------------------------------------------------------
+
+
+##dependencies
+##software
+##runs in python
+##postgres/gis (open geo suite)
+##the psycopg library
+##data
+
+##uses epsg 102010 - http://spatialreference.org/ref/esri/102010/
+##the output table "theTBL' variable below, needs to have the following fields to 
+##size_sort (integer)
+
+
+# Import system modules
+import sys, string, os
+import psycopg2
+import time
+from psycopg2.extras import RealDictCursor
+now = time.localtime(time.time())
+print "local time:", time.asctime(now)
+
+#variables
+myHost = "localhost"
+myPort = "5432"
+myUser = "postgres"
+db = "allison"
+
+	
+try:
+	#set up the connection to the database
+	myConn = "dbname=" + db + " host=" + myHost + " port=" + myPort + " user=" + myUser
+	conn = psycopg2.connect(myConn)
+	theCur = conn.cursor()
+	#get the total number of records to go through
+	theSQL = "SELECT ben" 
+	theSQL = theSQL + " FROM fabric.item24_cxns_mar28_expanded"
+	theSQL = theSQL + " GROUP BY ben;"
+	theCur.execute(theSQL)
+	print theSQL
+	driver = theCur.fetchall()
+	for r in driver:
+		theSQL2 = "SELECT * FROM fabric.nces_pub_ben"
+		theSQL2 = theSQL2 + " WHERE ben = '" + str(r[0]) + "'"
+		theSQL2 = theSQL2 + " AND size_sort IS NOT NULL"
+		theSQL2 = theSQL2 + " ORDER BY leaid, size_sort;"
+		theCur.execute(theSQL2)
+		print theSQL2
+		schools = theCur.fetchall()
+		maxschools = theCur.rowcount
+		theSQL3 = "SELECT * FROM fabric.item24_cxns_mar28_expanded"
+		theSQL3 = theSQL3 + " WHERE ben = '" + str(r[0]) + "'"
+		theSQL3 = theSQL3 + " ORDER BY download_speed DESC;"
+		theCur.execute(theSQL3)
+		print theSQL3
+		cxns = theCur.fetchall()
+		maxcxns = theCur.rowcount
+		for i,j in zip(schools, cxns):
+			print i
+			print j
+			theSQL4 = "UPDATE fabric.nces_pub_ben"
+			theSQL4 = theSQL4 + " SET type_cxn = '" + str(j[4]) + "'"
+			theSQL4 = theSQL4 + " WHERE school_id = '" + str(i[0]) + "'; commit; "
+			theCur.execute(theSQL4)		
+			print theSQL4
+			theSQL5 = "UPDATE fabric.nces_pub_ben"
+			theSQL5 = theSQL5 + " SET download_speed = " + str(j[6])
+			theSQL5 = theSQL5 + " WHERE school_id = '" + str(i[0]) + "'; commit;"
+			theCur.execute(theSQL5)
+			print theSQL5
+	theCur.close()
+	del theCur
+	del conn, myConn
+	now = time.localtime(time.time())
+	print "local time:", time.asctime(now)
+	
+except psycopg2.ProgrammingError as e:
+    # Err code lookup at http://www.postgresql.org/docs/9.1/static/errcodes-appendix.html
+    print "psycopg2 error code %s" % e.pgcode
+    raise e
---
+++ item24/school_matching.sql
@@ -0,0 +1,25 @@
+﻿select *
+from analysis.nces_pub_full;
+
+alter table analysis.nces_pub_full
+drop column if exists ben;
+
+select school_id, school_name, leaid, lea_name, lstate, school_loc, tot_students, rev_nces_id, applicant_type, ben
+into fabric.nces_pub_ben
+from analysis.nces_pub_full
+left join fabric.ben_nces_xwalk
+on ben_nces_xwalk.rev_nces_id = nces_pub_full.leaid;
+
+
+
+select school_id, COUNT(*) as myCount
+from fabric.nces_pub_ben
+where ben IS NOT NULL
+group by school_id
+order by myCount desc;
+--Rows: 118,332
+--Schools with BEN: 89,265
+
+select *
+from fabric.nces_pub_ben
+where school_id = '120039002468';
