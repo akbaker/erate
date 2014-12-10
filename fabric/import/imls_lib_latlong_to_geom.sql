7c93dfe3368a9024ba165ed21ab7a3b45fe4e96a
akbaker <FCC@fccs-mbp.fcc.gov>
2014-12-10 17:28:59 -0500

    Create geom

---
+++ fabric/import/imls_lib_latlong_to_geom.sql
@@ -0,0 +1,3 @@
+﻿ALTER table analysis.imls_lib_2012 ADD COLUMN geom geometry(POINT,4326);
+UPDATE analysis.imls_lib_2012 SET geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);
+CREATE INDEX imls_lib_geom_gist ON analysis.imls_lib_2012 USING GIST(geom);
