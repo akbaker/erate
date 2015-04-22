e95bebf0ea56b07d9034193a785f42a388ce0697
akbaker <FCC@fccs-mbp.fcc.gov>
2015-04-22 16:35:14 -0400

    Congressional districts

---
+++ fabric/cd113_schools.sql
@@ -0,0 +1,12 @@
+﻿--Add congressional district to schools tables
+alter table analysis.nces_public_2011
+add column cd113 character varying(2);
+
+with new_values as(
+select cd113fp, geom
+from census2010.cd113
+)
+update analysis.nces_public_2011
+set cd113 = cd113fp
+from new_values
+where st_within(nces_public_2011.geom, new_values.geom);
---
+++ fabric/import/import_cd113.sql
@@ -0,0 +1,28 @@
+﻿--CREATE TABLE
+DROP TABLE IF EXISTS census2010.cd113;
+CREATE TABLE census2010.cd113
+(
+	gid int,
+	statefp character varying(2),
+	cd113fp character varying(2),
+	geoid character varying(7),
+	namelsad character varying(41),
+	lsad character varying(2),
+	cdsessn character varying(3),
+	mtfcc character varying(5),
+	funcstat character varying(1),
+	aland double precision,
+	awater double precision, 
+	intptlat character varying(11),
+	intptlon character varying(12),
+	geom geometry
+)	
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE census2010.cd113
+  OWNER TO postgres;
+
+COPY census2010.cd113
+	FROM '/Users/FCC/Documents/allison/E-rate analysis/Data/Census/congressional_districts.csv'
+	CSV HEADER DELIMITER '|';
