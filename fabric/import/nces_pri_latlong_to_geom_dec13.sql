5bd5504465b17d870853331bdd2d35dffaef70b1
akbaker <FCC@fccs-mbp.fcc.gov>
2015-04-22 16:32:10 -0400

    Add lat/lon to geom files

---
+++ fabric/import/nces_pri_latlong_to_geom_dec13.sql
@@ -0,0 +1,3 @@
+﻿ALTER table analysis.nces_pri ADD COLUMN geom geometry(POINT,4326);
+UPDATE analysis.nces_pri SET geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);
+CREATE INDEX nces_pri_geom_gist ON analysis.nces_pri USING GIST(geom);
---
+++ fabric/import/nces_pub_latlong_to_geom_dec13.sql
@@ -0,0 +1,3 @@
+﻿ALTER table analysis.nces_pub ADD COLUMN geom geometry(POINT,4326);
+UPDATE analysis.nces_pub SET geom = ST_SetSRID(ST_MakePoint(longitude,latitude),4326);
+CREATE INDEX nces_pub_geom_gist ON analysis.nces_pub USING GIST(geom);
